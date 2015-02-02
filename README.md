787479c077f5256a07c3fbe7bf2d234d3dccc9d7
silevitas <si@silevitas.com>
2015-02-02 11:25:31 +0000

    1.1
    v1.1: New responsive flavour!

--- README.md
+++ README.md
@@ -6,5 +6,9 @@ Add to your player, either through the Studio, Player Management API, or on-page
 
 [script src="//example.com/plugins/autoHeight.js"][/script]
 
+# Release notes
+v1.1, 2015-02-02: added window.onresize functionality to make it responsive
+v1.0, 2015-02-01: relies on data-video-id to be set in the player embed code
+
 # Requirements
 Requires the Brightcove Player, built upon the Video JS open-source player, available at [videojs.com](http://videojs.com).
--- autoHeight.js
+++ autoHeight.js
@@ -1,9 +1,11 @@
+// (c)2015 Si Levitas
 // Plugin provided as-is and is not supported by Brightcove in any way, shape or form.
+// v1.1, 2015-02-02: added window.onresize functionality to make it responsive
 // v1.0, 2015-02-01: relies on data-video-id to be set in the player embed code
+
 
 // This fixes a limitation in IE9, where if the console isn't open on page load, any attempts to log to the console breaks the script
 if (!window.console) console = {log: function() {}};
-
 console.log("AutoHeight Plugin loaded!");
 
 var player = videojs('player');
@@ -33,13 +35,23 @@ player.ready(function(){
 		console.log(MP4Renditions[1]);
 		// Divide the height by the width 
 		var ratio = MP4Renditions[1].height / MP4Renditions[1].width;
-		// Get the player's on-page width
-		var playerWidth = document.getElementById('player').offsetWidth;
-		console.log("Current Player Width: " + playerWidth);
-		// Calculate the appropriate height by multiplying the width by the ratio calculated above
-		var playerHeight = Math.round(playerWidth * ratio);
-		console.log("Calculated Player Height: " + playerHeight);
-		// Set the player height (specify pixels to be valid HTML5)
-		document.getElementById('player').style.height = (playerHeight+"px");
+		// Trigger resize
+		resize();
+		// Resize put into a function so that it can be called on window resize (to make it responsive)
+		function resize() {
+			console.log("Resize triggered...");
+			// Get the player's on-page width
+			var playerWidth = document.getElementById('player').offsetWidth;
+			console.log("Current Player Width: " + playerWidth);
+			// Calculate the appropriate height by multiplying the width by the ratio calculated above
+			var playerHeight = Math.round(playerWidth * ratio);
+			console.log("Calculated Player Height: " + playerHeight);
+			// Set the player height (specify pixels to be valid HTML5)
+			document.getElementById('player').style.height = (playerHeight+"px");
+		}
+		// Trigger the resize function when the window size changes
+		window.addEventListener('resize', function(event){
+  			resize();	
+		});
 	});
 });
